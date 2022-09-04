ce0b2aeb898a015984d4521b2e6dbe038c4d9550
hilellasry <Your new email hilellasry@gmail.com>
2022-09-04 21:20:47 +0300

    N

--- verbals.py
+++ verbals.py
@@ -6,5 +6,5 @@ nickname3 = "flor"
 voicetype = 0  #what voice is this (1 for male voice)(1 for female voice)
 
 #----------------------------------------------------------------
-
+ 
 listening_time = 10 #how long to listen for command(Seconds)
